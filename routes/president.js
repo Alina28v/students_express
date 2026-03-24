874863a0f3bec7430009d505bf65343c50f428c2
StadnukNazar <nazar.i.stadnyk@ukd.edu.ua>
2026-03-24 22:13:17 +0200

    Fix mapping of president rows in query result

--- routes/president.js
+++ routes/president.js
@@ -4,9 +4,9 @@ import db from '../db/connector.js';
 
 router.get('/', async function(req, res, next) {
   const president = await db.query('SELECT * FROM presidents');
-  const rowPresident = president.rows.map(s => {
+  const rowPresident = president.rows.map(p => {
     return {
-      ...s,
+      ...p,
       created_at_time: s.created_at.toLocaleTimeString(), 
       created_at_date: s.created_at.toLocaleDateString()
     }
